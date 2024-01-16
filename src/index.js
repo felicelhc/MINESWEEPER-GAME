500fccc875f90263bcd92d6bfec683ccf16d6ed0
Felice <fishfillet2fab4u@gmail.com>
2024-01-16 19:06:14 +0000

    variables, loop & function added
    - div .cells deleted
    - JS added

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
-let scoreCounter = document.querySelector(".score-no");
-scoreCounter.innerHTML = "Hello";
+const scoreCounter = document.querySelector(".score-no");
+const grid = document.querySelector(".grid");
+const endGameScreen = document.querySelector(".game-over-screen");
+const endGameText = document.querySelector(".game-over-text");
+const playAgain = document.querySelector("#play-again-btn");
+
+const totalCells = 100;
+const totalBombs = 90;
+const maxScore = 5;
+const bombList = [];
+
+let score = 0;
+
+for (let i = 1; i <= 100; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add(".cell");
+
+  cell.addEventListener("click", function () {});
+
+  grid.appendChild(cell);
+}
