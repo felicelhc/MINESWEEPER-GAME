50394ef8307a43ad97f47c934a577265b24301db
Felice <fishfillet2fab4u@gmail.com>
2024-01-16 19:40:18 +0000

    new functions added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const scoreCounter = document.querySelector(".score-no");
 const grid = document.querySelector(".grid");
 const endGameScreen = document.querySelector(".game-over-screen");
 const endGameText = document.querySelector(".game-over-text");
-const playAgain = document.querySelector("#play-again-btn");
+const playAgainBtn = document.querySelector("#play-again-btn");
 
 const totalCells = 100;
 const totalBombs = 90;
@@ -11,11 +11,49 @@ const bombList = [];
 
 let score = 0;
 
+function updateScore() {
+  score++;
+  scoreCounter.innerText = score.toString().padStart(5, "0");
+
+  if (score === maxScore) {
+    endGame();
+  }
+}
+
 for (let i = 1; i <= 100; i++) {
   const cell = document.createElement("div");
-  cell.classList.add(".cell");
+  cell.classList.add("cell");
+  cell.classList.add("cell-bomb");
 
-  cell.addEventListener("click", function () {});
+  cell.addEventListener("click", function () {
+    if (bombsList.includes(i)) {
+      cell.classList.add("cell-bomb");
+      endGame();
+    }
+    cell.classList.add("cell-clicked");
+    updateScore();
+  });
 
   grid.appendChild(cell);
 }
+
+while (bombsLIst.length < totalBombs) {
+  const randomNumber = Math.floor(Math.random() * totalCells) + 1;
+
+  if (!bombsList.includes(randomNumber)) {
+    bombsList.push(randomNumber);
+  }
+}
+
+function endGame(isVictory) {
+  if (isVictory) {
+    endGameText.innerHTML = "YOU <br /> WON";
+    endGameScreen.classList.add("win");
+  }
+
+  endGameScreen.classList.remove("hidden");
+}
+
+playAgainBtn.addEventListener("click", function () {
+  window.location.reload();
+});
